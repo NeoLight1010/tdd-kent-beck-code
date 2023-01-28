c670d2a09e18af816e0f6d47393e459cc4b3e7e9
Anthony Suárez <neolight1010@gmail.com>
2023-01-28 08:50:07 -0500

    Add TODO list to the end of main.ts

--- main.ts
+++ main.ts
@@ -9,3 +9,12 @@ export class Dollar {
     return new Dollar(this.amount * multiplier);
   }
 }
+
+
+// TODO 
+// 
+// - [ ] $5 + 10 CHF = $10 if rate is 2:1
+// - [x] $5 * 2 = $10
+// - [ ] Make `amount` private.
+// - [x] Dollar side effects?
+// - [ ] Money rounding?
