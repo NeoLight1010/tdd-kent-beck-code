b10bb7f6e38e20093f4c655d427f649fd54340a3
Anthony Suárez <neolight1010@gmail.com>
2023-01-27 20:27:26 -0500

    c1: Assign new value in Dollar.times

--- main.ts
+++ main.ts
@@ -2,10 +2,12 @@ export class Dollar {
   amount: number;
 
   constructor(amount: number) {
-    this.amount = amount * 2;
+    this.amount = amount;
   }
 
-  times(multiplier: number): void {}
+  times(multiplier: number): void {
+    this.amount = this.amount * 2;
+  }
 }
 
 if (import.meta.main) {
