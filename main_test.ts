c8d8648d6d19e31a36a8c469d467af6f1b21725a
Anthony Suárez <neolight1010@gmail.com>
2023-02-02 08:21:45 -0500

    c8: Delete direct instances of Franc in tests

--- main_test.ts
+++ main_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.174.0/testing/asserts.ts";
-import { Franc, Money } from "./main.ts";
+import { Money } from "./main.ts";
 
 Deno.test(function testMultiplication() {
   const five: Money = Money.dollar(5);
@@ -12,15 +12,15 @@ Deno.test(function testEquals() {
   assertEquals((Money.dollar(5)).equals(Money.dollar(5)), true);
   assertEquals((Money.dollar(5)).equals(Money.dollar(6)), false);
 
-  assertEquals((new Franc(5)).equals(new Franc(5)), true);
-  assertEquals((new Franc(5)).equals(new Franc(6)), false);
+  assertEquals((Money.franc(5)).equals(Money.franc(5)), true);
+  assertEquals((Money.franc(5)).equals(Money.franc(6)), false);
 
-  assertEquals((new Franc(5)).equals(Money.dollar(5)), false);
+  assertEquals((Money.franc(5)).equals(Money.dollar(5)), false);
 });
 
 Deno.test(function testFrancMultiplication() {
-  const five = new Franc(5);
+  const five = Money.franc(5);
 
-  assertEquals(five.times(2).equals(new Franc(10)), true);
-  assertEquals(five.times(3).equals(new Franc(15)), true);
+  assertEquals(five.times(2).equals(Money.franc(10)), true);
+  assertEquals(five.times(3).equals(Money.franc(15)), true);
 });
