0c83dba8125a6aa2bb3e6878ef2e432e25829331
Anthony Suárez <neolight1010@gmail.com>
2023-02-02 08:19:06 -0500

    c8: Eliminate direct instances of Dollar in tests
    Hiding concrete subclasses allows us to change inheritance without
    affecting client code. We achieved this using factory methods.

--- main_test.ts
+++ main_test.ts
@@ -1,21 +1,21 @@
 import { assertEquals } from "https://deno.land/std@0.174.0/testing/asserts.ts";
-import { Dollar, Franc, Money } from "./main.ts";
+import { Franc, Money } from "./main.ts";
 
 Deno.test(function testMultiplication() {
   const five: Money = Money.dollar(5);
 
-  assertEquals(five.times(2).equals(new Dollar(10)), true);
-  assertEquals(five.times(3).equals(new Dollar(15)), true);
+  assertEquals(five.times(2).equals(Money.dollar(10)), true);
+  assertEquals(five.times(3).equals(Money.dollar(15)), true);
 });
 
 Deno.test(function testEquals() {
-  assertEquals((new Dollar(5)).equals(new Dollar(5)), true);
-  assertEquals((new Dollar(5)).equals(new Dollar(6)), false);
+  assertEquals((Money.dollar(5)).equals(Money.dollar(5)), true);
+  assertEquals((Money.dollar(5)).equals(Money.dollar(6)), false);
 
   assertEquals((new Franc(5)).equals(new Franc(5)), true);
   assertEquals((new Franc(5)).equals(new Franc(6)), false);
 
-  assertEquals((new Franc(5)).equals(new Dollar(5)), false);
+  assertEquals((new Franc(5)).equals(Money.dollar(5)), false);
 });
 
 Deno.test(function testFrancMultiplication() {
