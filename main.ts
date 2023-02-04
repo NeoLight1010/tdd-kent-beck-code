d58987bb2f8c284b09b685165452ca76e64d7320
Anthony Suárez <neolight1010@gmail.com>
2023-02-04 09:45:47 -0500

    c10: Push times methods up to Money

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@ export class Money {
   }
 
   times(multiplier: number): Money {
-    return new Money(0, "");
+    return new Money(this.amount * multiplier, this._currency);
   }
 
   currency(): string {
@@ -36,15 +36,9 @@ export class Money {
 }
 
 export class Dollar extends Money {
-  times(multiplier: number): Money {
-    return new Money(this.amount * multiplier, this._currency);
-  }
 }
 
 export class Franc extends Money {
-  times(multiplier: number): Money {
-    return new Money(this.amount * multiplier, this._currency);
-  }
 }
 
 // TODO
@@ -61,7 +55,7 @@ export class Franc extends Money {
 // - [x] 5 CHF * 2 = 10 CHF
 // - [ ] Dollar/Franc duplication
 // - [x] Common equals
-// - [ ] Common times
+// - [x] Common times
 // - [x] Compare Francs with Dollars
 // - [x] Currency?
 // - [ ] Delete testFrancMultiplication?
