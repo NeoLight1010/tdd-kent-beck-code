e13bbf89f70ef1885706eb79ddbf6c69c3799a49
Anthony Suárez <neolight1010@gmail.com>
2023-02-17 22:07:30 -0500

    c18: Move up run method to TestCase
    run() was only using fields from TestCase. Keep the operations near the
    data.

--- xunit/main.py
+++ xunit/main.py
@@ -2,15 +2,15 @@ class TestCase:
     def __init__(self, name: str) -> None:
         self.name = name
 
+    def run(self) -> None:
+        method = getattr(self, self.name)
+        method()
+
 class WasRun(TestCase):
     def __init__(self, name: str) -> None:
         self.was_run = None
 
         super().__init__(name)
-
-    def run(self) -> None:
-        method = getattr(self, self.name)
-        method()
 
     def test_method(self) -> None:
         self.was_run = 1
