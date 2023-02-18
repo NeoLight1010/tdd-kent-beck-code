a503665166a96395890ab499c6f5a48771c55522
Anthony Suárez <neolight1010@gmail.com>
2023-02-17 22:02:36 -0500

    c18: Make tests "pass" by hardwiring implementation

--- xunit/main.py
+++ xunit/main.py
@@ -2,6 +2,9 @@ class WasRun:
     def __init__(self, name: str) -> None:
         self.was_run = None
 
+    def run(self) -> None:
+        self.test_method()
+
     def test_method(self) -> None:
         self.was_run = 1
 
