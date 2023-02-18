5cc6a617ef08ed5d39547c36f4df1126ce409cf7
Anthony Suárez <neolight1010@gmail.com>
2023-02-17 22:04:45 -0500

    c18: Generalize test method execution
    The concrete example that TDD helped us create serves as a base for
    generalizing with less mental work.

--- xunit/main.py
+++ xunit/main.py
@@ -1,9 +1,11 @@
 class WasRun:
     def __init__(self, name: str) -> None:
+        self.name = name
         self.was_run = None
 
     def run(self) -> None:
-        self.test_method()
+        method = getattr(self, self.name)
+        method()
 
     def test_method(self) -> None:
         self.was_run = 1
