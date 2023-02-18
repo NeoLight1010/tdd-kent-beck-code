f516163573bf706c0dcc7a6d740e5469af59cc57
Anthony Suárez <neolight1010@gmail.com>
2023-02-17 22:00:13 -0500

    c18: Fix runtime errors of first test

--- xunit/main.py
+++ xunit/main.py
@@ -1,3 +1,10 @@
+class WasRun:
+    def __init__(self, name: str) -> None:
+        self.was_run = None
+
+    def test_method(self) -> None:
+        pass
+
 test = WasRun("test_method")
 print(test.was_run)
 test.test_method()
