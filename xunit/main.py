d9d8984d9fe6b2c972f16705f80fd9a1a8af1a18
Anthony Suárez <neolight1010@gmail.com>
2023-02-22 13:26:04 -0500

    c20: Rename test_set_up and remove test_running
    test_set_up was doing two jobs, so we renamed it and deleted the
    redundant test.

--- xunit/main.py
+++ xunit/main.py
@@ -26,16 +26,11 @@ class TestCaseTest(TestCase):
     def set_up(self) -> None:
         self.test = WasRun("test_method")
 
-    def test_running(self) -> None:
-        self.test.run()
-        assert self.test.was_run
-
-    def test_set_up(self) -> None:
+    def test_template_method(self) -> None:
         self.test.run()
         assert self.test.log == "set_up test_method "
 
-TestCaseTest("test_running").run()
-TestCaseTest("test_set_up").run()
+TestCaseTest("test_template_method").run()
 
 # TODO
 # - [x] Invoke test method
