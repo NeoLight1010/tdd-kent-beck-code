9dc14d1837c4492e43163550ea5b379793aa4cee
Anthony Suárez <neolight1010@gmail.com>
2023-02-22 13:28:17 -0500

    c20: Modify test_template_method to test tear_down

--- xunit/main.py
+++ xunit/main.py
@@ -26,7 +26,7 @@ class TestCaseTest(TestCase):
     def test_template_method(self) -> None:
         test = WasRun("test_method")
         test.run()
-        assert test.log == "set_up test_method "
+        assert test.log == "set_up test_method tear_down "
 
 TestCaseTest("test_template_method").run()
 
