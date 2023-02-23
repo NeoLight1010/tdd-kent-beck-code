716a22032d80c52c9abf39963493df73d5d3656e
Anthony Suárez <neolight1010@gmail.com>
2023-02-23 11:59:38 -0500

    c21: Write a test for result summary

--- xunit/main.py
+++ xunit/main.py
@@ -37,7 +37,13 @@ class TestCaseTest(TestCase):
         test.run()
         assert test.log == "set_up test_method tear_down "
 
+    def test_result(self) -> None:
+        test = WasRun("test_method")
+        result = test.run()
+        assert result.summary() == "1 run, 0 failed"
+
 TestCaseTest("test_template_method").run()
+TestCaseTest("test_result").run()
 
 # TODO
 # - [x] Invoke test method
