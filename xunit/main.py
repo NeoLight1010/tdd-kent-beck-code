264ad3ba97d4bfa782ec71433c2841706b958b8b
Anthony Suárez <neolight1010@gmail.com>
2023-02-24 18:50:05 -0500

    c23: Fix failing tests due to changed interface

--- xunit/main.py
+++ xunit/main.py
@@ -71,17 +71,26 @@ class WasRun(TestCase):
 class TestCaseTest(TestCase):
     def test_template_method(self) -> None:
         test = WasRun("test_method")
-        test.run()
+
+        result = TestResult()
+        test.run(result)
+
         assert test.log == "set_up test_method tear_down "
 
     def test_result(self) -> None:
         test = WasRun("test_method")
-        result = test.run()
+
+        result = TestResult()
+        test.run(result)
+
         assert result.summary() == "1 run, 0 failed"
 
     def test_failed_result(self) -> None:
         test = WasRun("test_broken_method")
-        result = test.run()
+
+        result = TestResult()
+        test.run(result)
+
         assert result.summary() == "1 run, 1 failed"
 
     def test_failed_result_formatting(self) -> None:
